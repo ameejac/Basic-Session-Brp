fb791de62bcf2c7a90326c18a1184bcdc9859c8e
Ameejac <ameejacherukuru123@gmail.com>
2022-03-22 20:29:04 +0530

    Ameeja[Added:Operators And Conversions Practice Code]

---
+++ BasicsDemoSession/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BasicsDemoSession
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //S2_VariablesDatatypes objS2 = new S2_VariablesDatatypes();
+            //objS2.VariablesAndUses();
+
+            //S3_operatorsandconversions
+            S3_operatorsandconversions objS3 = new S3_operatorsandconversions();
+            objS3.OperatorAndConversion();
+        }
+    }
+}
