3b7c03711ef1deeccbc94014ea8bc845874c4e94
Ameejac <ameejacherukuru123@gmail.com>
2022-03-22 20:07:06 +0530

    Ameeja[Added:Variables and Datatypes Practice Code]

---
+++ BasicsDemoSession/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace BasicsDemoSession
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            S2_VariablesDatatypes objS2 = new S2_VariablesDatatypes();
+            objS2.VariablesAndUses();
+        }
+    }
+}
